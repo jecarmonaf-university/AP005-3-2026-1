e71aececad4312b6c1ea350e33af8ebf29bc8111
jecarmonaf-university <carmonajuan.university@gmail.com>
2026-02-10 09:51:35 -0500

    Add files via upload

--- Corte1/Teoria/Practica2/Practica2.py
+++ Corte1/Teoria/Practica2/Practica2.py
@@ -1,9 +1,6 @@
 n1 = 3
 n2 = 3.0
 n3 = "3"
-print("n1 es: ")
-print(type(n1))
-print("n2 es: ")
-print(type(n2))
-print("n3 es: ")
-print(type(n3))
+print("n1 es: ", n1 , "y su tipo es: ", (type(n1)))
+print("n1 es: ", n2 , "y su tipo es: ",type(n2))
+print("n1 es: ", n3 , "y su tipo es: ",type(n3))
